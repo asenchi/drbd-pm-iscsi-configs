1612b524670c4d5e860efe3830307ee9bec86235
Curt Micol <asenchi@asenchi.com>
2010-07-20 14:44:32 -0400

    rpm listing to install iozone. Kept for reference.

---
+++ drbd-pacemaker/blade/installing-iozone.sh
@@ -0,0 +1,7 @@
+rpm -ivh http://mirror.centos.org/centos/5.5/os/x86_64/CentOS/libXpm-3.5.5-3.x86_64.rpm
+rpm -ivh http://mirror.centos.org/centos/5.5/os/x86_64/CentOS/pkgconfig-0.21-2.el5.x86_64.rpm
+rpm -ivh http://mirror.centos.org/centos/5.5/os/x86_64/CentOS/gd-2.0.33-9.4.el5_4.2.x86_64.rpm
+rpm -ivh http://mirror.centos.org/centos/5.5/os/x86_64/CentOS/perl-HTML-Tagset-3.10-2.1.1.noarch.rpm
+rpm -ivh http://mirror.centos.org/centos/5.5/os/x86_64/CentOS/perl-HTML-Parser-3.55-1.fc6.x86_64.rpm
+rpm -ivh ftp://mirror.switch.ch/pool/3/mirror/centos/5.5/os/x86_64/CentOS/gnuplot-4.0.0-14.el5.x86_64.rpm
+rpm -ivh ftp://ftp.pbone.net/mirror/centos.karan.org/el5/extras/testing/x86_64/RPMS/iozone-3-3.el5.kb.x86_64.rpm
